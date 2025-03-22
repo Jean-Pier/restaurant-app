ba1ace92af5f1c28c693d334709adc3fe548c607
Jean-Pier <jeanpierre5665@gmail.com>
2025-03-22 00:10:45 -0500

    fix : add google maps
    .

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:restaurant_app/core/api_service.dart';
+import 'package:restaurant_app/core/location_service.dart';
 import 'package:restaurant_app/screens/panel/panel_screen.dart';
 import 'package:restaurant_app/widgets/appbar.dart';
 import 'package:restaurant_app/widgets/icon_text.dart';
@@ -13,16 +16,25 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final apiService = ApiService();
+  GoogleMapController? _mapController;
+  LatLng? _currentPosition;
   @override
   void initState() {
     super.initState();
     checkStatus();
+    _fetchLocation();
   }
 
   @override
   void dispose() {
+    _mapController?.dispose();
     super.dispose();
   }
+
+  static const CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(-12.046093, -77.030690),
+    zoom: 12,
+  );
 
   checkStatus() async {
     final response = await apiService.checkApiStatus();
@@ -33,34 +45,59 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  Future<void> _fetchLocation() async {
+    try {
+      Position? position = await LocationService.getCurrentLocation();
+      if (position != null) {
+        setState(() {
+          _currentPosition = LatLng(position.latitude, position.longitude);
+        });
+
+        _mapController?.animateCamera(
+          CameraUpdate.newLatLngZoom(_currentPosition!, 15),
+        );
+      }
+    } catch (e) {
+      debugPrint("Error obteniendo ubicación: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidgets(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            IconTextWidgets(label: 'Home'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Flexible(child: Text("Total: 8")),
-                SizedBox(width: 20),
-                Flexible(child: Text("Pendientes: 5")),
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const PanelScreen()),
-                );
+      body: Column(
+        children: [
+          IconTextWidgets(label: 'Home'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Flexible(child: Text("Total: 8")),
+              SizedBox(width: 20),
+              Flexible(child: Text("Pendientes: 5")),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const PanelScreen()),
+              );
+            },
+            child: Text('next'),
+          ),
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition:_initialPosition,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              onMapCreated: (GoogleMapController controller) {
+                _mapController = controller;
               },
-              child: Text('next'),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
